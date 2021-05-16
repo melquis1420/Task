a77ce0eff026f1403c1be4ba76d995a817e36ace
melquis1420 <melqui1420@gmail.com>
2021-05-15 22:54:44 -0300

    Project finalization
    The implementations are completed and the project finalized.

--- app/src/main/java/com/example/tasks/view/AllTasksFragment.kt
+++ app/src/main/java/com/example/tasks/view/AllTasksFragment.kt
@@ -5,12 +5,10 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.TextView
 import android.widget.Toast
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProvider
-import androidx.lifecycle.ViewModelProviders
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 import com.example.tasks.R
@@ -24,10 +22,15 @@ class AllTasksFragment : Fragment() {
     private lateinit var mViewModel: AllTasksViewModel
     private lateinit var mListener: TaskListener
     private val mAdapter = TaskAdapter()
+    private var mTaskFilter = 0
 
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, s: Bundle?): View {
         mViewModel = ViewModelProvider(this).get(AllTasksViewModel::class.java)
         val root = inflater.inflate(R.layout.fragment_all_tasks, container, false)
+
+
+        //Task filter
+        mTaskFilter= requireArguments().getInt(TaskConstants.BUNDLE.TASKFILTER,0)
 
         val recycler = root.findViewById<RecyclerView>(R.id.recycler_all_tasks)
         recycler.layoutManager = LinearLayoutManager(context)
@@ -67,13 +70,13 @@ class AllTasksFragment : Fragment() {
     override fun onResume() {
         super.onResume()
         mAdapter.attachListener(mListener)
-        mViewModel.list()
+        mViewModel.list(mTaskFilter)
     }
 
     private fun observe() {
         mViewModel.tasks.observe(viewLifecycleOwner, Observer {
             if (it.count() > 0) {
-                mAdapter.updateListener(it)
+                mAdapter.updateList(it)
             }
         })
 
--- app/src/main/java/com/example/tasks/viewmodel/AllTasksViewModel.kt
+++ app/src/main/java/com/example/tasks/viewmodel/AllTasksViewModel.kt
@@ -5,6 +5,7 @@ import androidx.lifecycle.AndroidViewModel
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
+import com.example.tasks.service.constants.TaskConstants
 import com.example.tasks.service.listener.APIListener
 import com.example.tasks.service.listener.ValidationListener
 import com.example.tasks.service.model.PriorityModel
@@ -14,6 +15,7 @@ import com.example.tasks.service.repository.TaskRepository
 class AllTasksViewModel(application: Application) : AndroidViewModel(application) {
 
     private val mTaskRepository = TaskRepository(application)
+    private var mTaskFilter = 0
 
     private val mValidation = MutableLiveData<ValidationListener>()
     var validation: LiveData<ValidationListener> = mValidation
@@ -21,9 +23,10 @@ class AllTasksViewModel(application: Application) : AndroidViewModel(application
     private val mList = MutableLiveData<List<TaskModel>>()
     var tasks: LiveData<List<TaskModel>> = mList
 
-    fun list(){
+    fun list(taskFilter: Int) {
+        mTaskFilter = taskFilter
 
-        mTaskRepository.all(object :APIListener<List<TaskModel>>{
+        val listener = object : APIListener<List<TaskModel>> {
             override fun onSuccess(model: List<TaskModel>) {
                 mList.value = model
             }
@@ -33,40 +36,52 @@ class AllTasksViewModel(application: Application) : AndroidViewModel(application
                 mValidation.value = ValidationListener(str)
             }
 
-        })
+        }
+
+        if (mTaskFilter == TaskConstants.FILTER.ALL){
+            mTaskRepository.all(listener)
+        }else if (mTaskFilter == TaskConstants.FILTER.NEXT){
+            mTaskRepository.nextWeek(listener)
+        }else{
+            mTaskRepository.overdue(listener)
+
+        }
+
+
 
     }
 
-    fun delete(id: Int){
-        mTaskRepository.delete(id, object : APIListener<Boolean>{
+    fun delete(id: Int) {
+
+        mTaskRepository.delete(id, object : APIListener<Boolean> {
             override fun onSuccess(model: Boolean) {
-                list()
-                mValidation.value= ValidationListener()
+                list(mTaskFilter)
+                mValidation.value = ValidationListener()
             }
 
             override fun onFailure(str: String) {
-                mValidation.value= ValidationListener(str)
+                mValidation.value = ValidationListener(str)
 
             }
 
         })
     }
 
-    fun complete(id: Int){
+    fun complete(id: Int) {
         updateStatus(id, true)
 
     }
 
 
-    fun undo(id: Int){
+    fun undo(id: Int) {
         updateStatus(id, false)
 
     }
 
-    private fun updateStatus(id: Int, complete: Boolean){
-        mTaskRepository.updateStatus(id, complete, object : APIListener<Boolean>{
+    private fun updateStatus(id: Int, complete: Boolean) {
+        mTaskRepository.updateStatus(id, complete, object : APIListener<Boolean> {
             override fun onSuccess(model: Boolean) {
-                list()
+                list(mTaskFilter)
             }
 
             override fun onFailure(str: String) {
